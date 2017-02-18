f01178c1e8cd6c8087e947641f97810d32d622e8
eyadkht <eyad.kht@hotmail.com>
2017-02-18 21:25:59 +0200

    Add Stream Controller with Views

---
+++ Bootstrap3/Controllers/StreamController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bootstrap3.Controllers
+{
+    public class StreamController : Controller
+    {
+        // GET: Stream
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BuildTwitterStream()
+        {
+
+            return View();
+        }
+
+        public ActionResult ViewSavedTwitterStreams()
+        {
+
+            return View();
+        }
+    }
+}
