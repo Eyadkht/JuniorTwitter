a87092d245a0b056753ad0c03afd19c16b427fef
eyadkht <eyad.kht@hotmail.com>
2017-02-18 22:13:18 +0200

    Add Live Tweets Controller With Views

---
+++ Bootstrap3/Controllers/LiveTweetsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bootstrap3.Controllers
+{
+    public class LiveTweetsController : Controller
+    {
+        // GET: LiveTweets
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+    }
+}
